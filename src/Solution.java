2456df7199a411fcfc4653d758795287365a373d
Arvis Abromovics <info@vsmedio.lv>
2018-01-06 22:17:23 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,50 @@
+/*
+Кошачья бойня
+Создать три кота используя класс Cat.
+Провести три боя попарно между котами.
+Класс Cat создавать не надо. Для боя использовать метод boolean fight(Cat anotherCat).
+Результат каждого боя вывести на экран c новой строки.
+
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. Нужно создать три объекта типа Cat.
+3. Нужно провести три боя.
+4. Программа должна вывести результат каждого боя с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat1 = new Cat ("Barsik",3,5,10);
+        Cat cat2 = new Cat ("Zanuda",7,9,20);
+        Cat cat3 = new Cat ("Pipec",2,2,3);
+
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat2.fight(cat3));
+        System.out.println(cat1.fight(cat3));
+    }
+
+    public static class Cat {
+        protected String name;
+        protected int age;
+        protected int weight;
+        protected int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            int agePlus = this.age > anotherCat.age ? 1 : 0;
+            int weightPlus = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthPlus = this.strength > anotherCat.strength ? 1 : 0;
+
+            int score = agePlus + weightPlus + strengthPlus;
+            return score > 2; // return score > 2 ? true : false;
+        }
+    }
+}
